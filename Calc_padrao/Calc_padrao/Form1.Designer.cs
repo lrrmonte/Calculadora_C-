1e38b6003ec21971b715a646f31423f780e3af0a
lrrmonte <lincoln.rubim@gmail.com>
2022-09-26 12:43:36 -0300

    Calc

--- Calc_padrao/Calc_padrao/Form1.Designer.cs
+++ Calc_padrao/Calc_padrao/Form1.Designer.cs
@@ -294,7 +294,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Calculadora";
+            this.Text = "CalculadoraC#";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
